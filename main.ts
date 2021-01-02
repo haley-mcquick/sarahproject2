41e984130af07b78cad9dd010386d7593e03c84a
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-02 15:56:07 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -2,11 +2,10 @@ let varLight: number;
 while (true) {
     console.log(input.lightLevel())
     varLight = input.lightLevel()
+    if (varLight < 5) {
+        //  counter -= 1
+        light.setAll(color.rgb(255, 0, 0))
+        light.setLength(30)
+    }
+    
 }
-//  counter = 1
-if (varLight < 5) {
-    //  counter -= 1
-    light.setAll(color.rgb(255, 0, 0))
-    light.setLength(30)
-}
-
