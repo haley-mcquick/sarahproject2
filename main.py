ceefd6c4dd72170e52d74b0a5b4e4faca701beae
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-02 14:31:24 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ while True:
     print(input.light_level())
     varLight = input.light_level()
 
-counter = 1
+# counter = 1
 if varLight < 5:
-    counter -= 1
-
-if counter < 1:
+    # counter -= 1
     light.set_all(color.rgb(255, 0, 0))
+    light.set_length(30)
+
+# if counter < 1:
+#     light.set_all(color.rgb(255, 0, 0))
--- main.ts
+++ main.ts
@@ -3,12 +3,10 @@ while (true) {
     console.log(input.lightLevel())
     varLight = input.lightLevel()
 }
-let counter = 1
+//  counter = 1
 if (varLight < 5) {
-    counter -= 1
-}
-
-if (counter < 1) {
+    //  counter -= 1
     light.setAll(color.rgb(255, 0, 0))
+    light.setLength(30)
 }
 
